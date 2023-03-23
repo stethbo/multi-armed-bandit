862b7a226acd66da7bb87f975e35e829684aca98
stethbo <sztefan.borek@gmail.com>
2023-03-23 09:50:11 +0100

    updates badnit

--- bandit.py
+++ bandit.py
@@ -5,31 +5,39 @@ logging.basicConfig(level=logging.INFO,
                     format="%(levelname)s %(message)s")
 
 class Bandit:
-    def __init__(self, prob: int) -> None:
-        self.prob = prob / 10
-        self.score = 0
+    def __init__(self, probs: list[float]) -> None:
+        '''
+        :param list probs: probabilities of following bandits
+            len(probs) >= 2
+            probs[i] in <0, 1>
+        '''
+        assert len(probs) > 2, 'List must be longer than 2'
+        self.probs = probs
+        self.scores = [0] * len(probs)
 
-    def pull(self):
-        if np.random.uniform(0, 1) < self.prob:
-            self.score += 1
-            return 1
-        else:
-            self.score -= 1
-            return -1
+
+    def pull(self, i: int) -> int:
+        '''
+        :param int i: i'th bandit to be used
+        '''
+        return 1 if np.random.uniform(0, 1) < self.probs[i] else 0
         
-    def get_info(self):
-        return self.prob, self.score
+
+if __name__ == '__main__':
+    bandits_probs = np.random.randint(2, 10, 10) / 10
     
+    mab = Bandit(bandits_probs)
 
-bandit_a = Bandit(np.random.randint(2, 8))
-bandit_b = Bandit(np.random.randint(2, 8))
-bandit_c = Bandit(np.random.randint(2, 8))
+    rounds = 25
+    start_index = 0
+    total_score = 0
+    for _ in range(rounds):
+        score = mab.pull(start_index)
+        if score == 0:
+            index = np.random.randint(0, len(bandits_probs))
+        else:
+            total_score += 1
 
-for n in range(100):
-    bandit_a.pull()
-    bandit_b.pull()
-    bandit_c.pull()
-
-logging.info(f" {bandit_a.score=}, {bandit_a.prob=}")
-logging.info(f" {bandit_b.score=}, {bandit_b.prob=}")
-logging.info(f" {bandit_c.score=}, {bandit_c.prob=}")
+    logging.info(f"Overall accuracy {total_score/rounds}")
+    logging.info(f"Bandits probs: {bandits_probs}, {np.average(bandits_probs)}")
+    
